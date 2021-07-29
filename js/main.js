59096f13daba21b0ca8586bf648c4761e197ba9d
DESKTOP-8066J6E\Admin <vole3581@gmail.com>
2021-07-29 16:38:30 +0700

    changelayout

--- js/main.js
+++ js/main.js
@@ -1,36 +1,72 @@
-fetch('/html/components/Header.html')
-  .then(response => {
-    return response.text()
-  })
-  .then(data => {
-    document.querySelector("header").innerHTML = data;
-  });
-  // catch
-  fetch('/html/components/Imagebig.html')
-  .then(re => {
-    return re.text()
-  })
-  .then(dat => {
-    document.querySelector("imagebig").innerHTML = dat;
-  });
-fetch('/html/components/business.html')
-.then(business_body=>{
-    return business_body.text()
-})
-.then(data_business=>{
-    document.querySelector("businessareas").innerHTML=data_business
-})
-fetch('/html/components/Latestnews.html')
-.then(newss => {
-return newss.text();
-})
-.then(data_news=>{
-  document.querySelector("newsbody").innerHTML=data_news
-})
-fetch('/html/components/Event.html')
-.then(events=>{
-  return events.text();
-})
-.then(event_body=>{
-  document.querySelector("eventbody").innerHTML=event_body
-})
+
+var slideIndex=1;
+convertSlide(slideIndex);
+function plusSlide(n) {
+  convertSlide(slideIndex += n)
+}
+function convertSlide(n) {
+  var i;
+  var slides = document.getElementsByClassName("image_single_big")
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+}
+
+ 
+var indexSlide = 1;
+changeSlide(indexSlide);
+function plusSlides(k) {
+  changeSlide(indexSlide += k)
+}
+function slideNews(k) {
+  changeSlide(indexSlide = k)
+}
+function changeSlide(k) {
+  var i;
+  var slideElement = document.getElementsByClassName("slide_event")
+  var classElement = document.getElementsByClassName("newschild")
+  if (k > classElement.length) { 
+    indexSlide = 1;
+  }
+  if (k < 1) {
+    indexSlide = classElement.length;
+  }
+  for (i = 0; i < classElement.length; i++) {
+    classElement[i].style.display = "none";
+  }
+  for (var i = 0; i < slideElement.length; i++) {
+    slideElement[i].className = slideElement[i].className.replace(" active", " ")
+  }
+
+  classElement[indexSlide-1].style.display = "block";
+  slideElement[indexSlide-1].className += " active"
+}
+var indexstart=0;
+var indexend=3;
+var indexEvent=1;
+slideEvent(indexEvent)
+function plusSlideEvent(h){
+slideEvent(indexEvent+=h)
+}
+function slideEvent(h){
+  var classEvent=document.getElementsByClassName("image_event_child")
+if(h > classEvent.length){
+  indexEvent=1;
+}
+if(h < 1){
+  indexEvent=classEvent.length
+}
+for(var i=indexend+1;i < classEvent.length;i++){
+  classEvent[i].style.display="none"
+}
+classEvent[indexEvent-1].style.display="block"
+}
+
